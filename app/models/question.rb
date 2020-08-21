6cbbb806e76d200db13608a78f86958cb02eced3
CJ Gilroy <c.gilroy9@gmail.com>
2020-08-21 13:46:25 -0600

    Working on question ersults method

--- app/models/question.rb
+++ app/models/question.rb
@@ -15,4 +15,25 @@ class Question < ApplicationRecord
     has_many :responses,
         through: :answer_choices,
         source: :responses
+
+    def results
+        # output = {}
+        # answer_choices.includes(:responses).each do |choice|
+        #     output[choice.text] = choice.responses.length
+        # end
+
+        output = AnswerChoice.find_by_sql([<<-SQL, id])
+            SELECT 
+                answer_choices.text, COUNT(responses.id) as answer_count
+            FROM
+                answer_choices
+            LEFT OUTER JOIN
+                responses ON answer_choices.id = responses.answer_choice_id
+            WHERE
+                answer_choices.question_id = ?
+            GROUP BY 
+                answer_choices.text
+        SQL
+        output
+    end
 end
