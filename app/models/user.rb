c110aad50f3454cd11220f25fd29de9d6907d58b
CJ Gilroy <c.gilroy9@gmail.com>
2020-08-25 12:12:36 -0600

    Add bonus method User#completed_polls

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,31 @@ class User < ApplicationRecord
         foreign_key: :respondent_id,
         primary_key: :id,
         class_name: 'Response'
+
+    def completed_polls
+        data = Poll.find_by_sql(<<-SQL)
+            SELECT 
+                polls.*
+            FROM
+                polls
+            JOIN
+                questions ON questions.poll_id = polls.id
+            JOIN
+                answer_choices ON answer_choices.question_id = questions.id
+            LEFT OUTER JOIN (
+                SELECT
+                    *
+                FROM
+                    responses
+                WHERE
+                    responses.respondent_id = #{self.id}
+            ) AS responses ON answer_choices.id = responses.answer_choice_id
+            GROUP BY 
+                polls.id
+            HAVING
+                COUNT(DISTINCT questions.id) = COUNT(responses.id)
+        SQL
+        data
+    end
+
 end
